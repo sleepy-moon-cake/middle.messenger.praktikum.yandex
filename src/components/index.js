13a164afd6b00c70547569893bb8f3451fe02aed
Alexey S <blueseasailordeveloper@gmail.com>
2022-04-25 00:30:38 +0700

    Implement profiles pages

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { registerFormComponent } from './form/form';
 export { registerInput } from './input/input';
 export { registerLink } from './link/link';
 export { registerPageError } from './page-error/page-error';
+export { registerField } from  './field/field';
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import {loginPageHTML} from './pages/login/login.js'
-import {signinPageHTML} from './pages/signin/signin.js';
+import { loginPageHTML } from './pages/login/login.js'
+import { signinPageHTML } from './pages/signin/signin.js';
 import { notFoundPageHTML } from './pages/errors/not-found/not-found.js';
-import {unavailablePageHTML} from './pages/errors/unavailable/unavailable.js';
+import { unavailablePageHTML } from './pages/errors/unavailable/unavailable.js';
+import { profilePageHTML } from './pages/profile/profile.js';
+import { editProfilePageHTML } from './pages/edit-profile/edit-profile.js';
+import { editPasswordPageHTML } from './pages/edit-password/edit-password.js';
 
 const root = document.getElementById('root');
 
-root.innerHTML = unavailablePageHTML();
+root.innerHTML = editPasswordPageHTML();

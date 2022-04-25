0c1c42ed3b8bce3362911d1ea3e776f978c8e08f
Alexey S <blueseasailordeveloper@gmail.com>
2022-04-25 18:13:57 +0700

    Setup server and tentative router

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express');
+import express from "express";
+import path from 'path';
+
+const __dirname = path.resolve();
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./src'))
+app.use(express.static(path.join(__dirname, '/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
 
 app.listen(PORT, () => {
-    console.log('Server has been starting')
-})
+  console.log(`Server has been starting on: ${PORT}!`);
+});
--- src/index.js
+++ src/index.js
@@ -8,6 +8,32 @@ import { profilePageHTML } from './pages/profile/profile.js';
 import { editProfilePageHTML } from './pages/edit-profile/edit-profile.js';
 import { editPasswordPageHTML } from './pages/edit-password/edit-password.js';
 
+const routes = {
+    '/':loginPageHTML,
+    '/login': loginPageHTML,
+    '/signin': signinPageHTML,
+    '/404':notFoundPageHTML,
+    '/505':unavailablePageHTML,
+    '/profile':profilePageHTML,
+    '/edit-profile':editProfilePageHTML,
+    '/edit-password':editPasswordPageHTML
+};
 const root = document.getElementById('root');
 
-root.innerHTML = editPasswordPageHTML();
+window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
+
+function router(event){
+    const pahtname = window.location.pathname
+    const page = routes[pahtname];
+
+    if(page){
+        renderPage(page);
+    }else {
+        renderPage(routes["/404"])
+    }
+}
+
+function renderPage(page){
+    root.innerHTML = page();
+}

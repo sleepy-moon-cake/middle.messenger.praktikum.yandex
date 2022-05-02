adbd8e61c06b4e16d340e74699c62e007fa1b11b
Alexey S <blueseasailordeveloper@gmail.com>
2022-05-02 21:04:23 +0700

    Setup stylelint, eslint, prettier

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
-import commonStyles from '../static/styles/common.scss';
-
-import { loginPageHTML } from './pages/login/login.js'
-import { signinPageHTML } from './pages/signin/signin.js';
-import { notFoundPageHTML } from './pages/errors/not-found/not-found.js';
-import { unavailablePageHTML } from './pages/errors/unavailable/unavailable.js';
-import { profilePageHTML } from './pages/profile/profile.js';
-import { editProfilePageHTML } from './pages/edit-profile/edit-profile.js';
-import { editPasswordPageHTML } from './pages/edit-password/edit-password.js';
+import commonStyles from "../static/styles/common.scss";
+import { editPasswordPageHTML } from "./pages/edit-password/edit-password";
+import { editProfilePageHTML } from "./pages/edit-profile/edit-profile";
+import { notFoundPageHTML } from "./pages/errors/not-found/not-found";
+import { unavailablePageHTML } from "./pages/errors/unavailable/unavailable";
+import { loginPageHTML } from "./pages/login/login";
+import { profilePageHTML } from "./pages/profile/profile";
+import { signinPageHTML } from "./pages/signin/signin";
 
 const routes = {
-    '/':loginPageHTML,
-    '/login': loginPageHTML,
-    '/signin': signinPageHTML,
-    '/404':notFoundPageHTML,
-    '/505':unavailablePageHTML,
-    '/profile':profilePageHTML,
-    '/edit-profile':editProfilePageHTML,
-    '/edit-password':editPasswordPageHTML
+  "/": loginPageHTML,
+  "/login": loginPageHTML,
+  "/signin": signinPageHTML,
+  "/404": notFoundPageHTML,
+  "/505": unavailablePageHTML,
+  "/profile": profilePageHTML,
+  "/edit-profile": editProfilePageHTML,
+  "/edit-password": editPasswordPageHTML,
 };
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
-window.addEventListener('load', router);
-window.addEventListener('hashchange', router);
+window.addEventListener("load", router);
+window.addEventListener("hashchange", router);
 
-function router(event){
-    const pahtname = window.location.pathname
-    const page = routes[pahtname];
+function router(event) {
+  const pahtname = window.location.pathname;
+  const page = routes[pahtname];
 
-    if(page){
-        renderPage(page);
-    }else {
-        renderPage(routes["/404"])
-    }
+  if (page) {
+    renderPage(page);
+  } else {
+    renderPage(routes["/404"]);
+  }
 }
 
-function renderPage(page){
-    root.innerHTML = page();
+function renderPage(page) {
+  root.innerHTML = page();
 }

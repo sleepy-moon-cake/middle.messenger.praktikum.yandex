22185e14b8f141fe9dac8554a3e7e69f4760c11d
Alexey S <blueseasailordeveloper@gmail.com>
2022-07-19 14:50:05 +0700

    Add enum with url paths

---
+++ src/models/enums/routes.ts
@@ -0,0 +1,11 @@
+export enum Routes {
+  ROOT = "/",
+  SIGNIN = "/signin",
+  SIGNUP = "/signup",
+  CHATS = "/chats",
+  NOT_FOUND = "/404",
+  UNAVALIABLE = "/505",
+  PROFILE = "/profile",
+  EDIT_PROFILE = "/edit-profile",
+  EDIT_PASSWORD = "/edit-password",
+}

f1621324c67aa9f7b6752b1d4f2d25ecaf97013c
Alexey S <blueseasailordeveloper@gmail.com>
2022-07-19 14:48:07 +0700

    Remove redundant files

--- src/pages/authentication/authentication.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "../../core/component/component";
-import { template } from "./authentication.tmpl";
-
-export class Authentication extends Component {
-  public render() {
-    return this.compile(template, this._props);
-  }
-}

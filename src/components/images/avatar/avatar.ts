a5f8e99fcad3548aa5421f112a8d394066e6e177
Alexey S <blueseasailordeveloper@gmail.com>
2022-07-21 19:39:19 +0700

    Change styles in avatar component

--- src/components/images/avatar/avatar.ts
+++ src/components/images/avatar/avatar.ts
@@ -2,6 +2,6 @@ import { Component } from "../../../core/component/component";
 
 export class Avatar extends Component {
   public render() {
-    return `<span class="avatar"><img src="{{src}}" alt="{{alt}}"></span>`;
+    return `<span class="avatar"><img src={{src}} alt={{alt}}></span>`;
   }
 }

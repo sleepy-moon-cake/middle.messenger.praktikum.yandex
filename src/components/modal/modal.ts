f8535921472f4fc7f64cc78641f6b1610c03d9e9
Alexey S <blueseasailordeveloper@gmail.com>
2022-07-21 22:24:53 +0700

    Add row Modal component

---
+++ src/components/modal/modal.ts
@@ -0,0 +1,10 @@
+import { Component } from "../../core/component/component";
+
+export class Modal extends Component {
+  render() {
+    return `<div class="modal">
+    <div class="modal__content">content</div>
+    <div class="modal__layout">content</div>
+    <div>`;
+  }
+}

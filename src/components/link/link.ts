7f4aeb4152090ebc0c682fe94d8cc8dfe5ed39bb
Alexey S <blueseasailordeveloper@gmail.com>
2022-07-21 11:54:38 +0700

    Change link component

--- src/components/link/link.ts
+++ src/components/link/link.ts
@@ -1,12 +1,30 @@
 import { Component } from "../../core/component/component";
 
-export type LinkData = {
-  class: string;
+export type LinkProps = {
   href: string;
   text: string;
+  rout: string;
 };
 
-export class Link extends Component<LinkData> {
+export class Link extends Component {
+  constructor({ href, text, rout }: LinkProps) {
+    const events = rout
+      ? {
+          click: (e: Event) => {
+            e.preventDefault();
+
+            window.router.go(rout);
+          },
+        }
+      : null;
+
+    super({
+      text,
+      href,
+      events,
+    });
+  }
+
   render() {
     return `<a class="link {{class}}" href="{{href}}">{{text}}</a>`;
   }

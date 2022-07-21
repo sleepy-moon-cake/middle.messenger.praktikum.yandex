fac0e8f6c0cd31e8edcfe2519f01f59e30e67107
Alexey S <blueseasailordeveloper@gmail.com>
2022-07-21 22:22:15 +0700

    Change Button compont to be able to use content projection

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -3,15 +3,21 @@ import { Component } from "../../core/component/component";
 export type ButtonProps = {
   type: string;
   text: string;
+  content: boolean;
   onClick: () => void;
 };
 
 export class Button extends Component {
-  constructor({ text, type, onClick }: ButtonProps) {
-    super({ text, type, events: { click: onClick } });
+  constructor({ content, text, type, onClick }: ButtonProps) {
+    super({ content, text, type, events: { click: onClick } });
   }
 
   public render(): string {
-    return '<button class="button" type="{{type}}">{{text}}</button>';
+    return `<button class="${
+      this.props.content ? "button--with-content" : "button"
+    }" type="{{type}}"> 
+      <div data-layout="1"></div>
+      {{text}} 
+    </button>`;
   }
 }
